9e1a7ce210be8166db051a2ec4ba73a67a9524ba
Jas Atwal <19190903+jas-atwal@users.noreply.github.com>
2019-06-26 18:52:26 +0100

    Create ci-install-hugo.sh

---
+++ ci-install-hugo.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+HUGO_DOWNLOAD=hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+
+set -x
+set -e
+
+wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_DOWNLOAD}
+tar xvzf ${HUGO_DOWNLOAD} hugo
+sudo mv hugo /usr/local/bin
+hugo server -D
